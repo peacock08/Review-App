13f88c4846c504ef3d9cf615895412cd51a79c69
Congvu <Cong080201@gmail.com>
2023-05-26 00:17:36 +0700

    third

--- app/src/main/java/com/example/myapplication/fragment/FragmentInfo.java
+++ app/src/main/java/com/example/myapplication/fragment/FragmentInfo.java
@@ -1,6 +1,11 @@
 package com.example.myapplication.fragment;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,11 +13,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
@@ -47,6 +52,15 @@ public class FragmentInfo extends Fragment {
             }
         });
 
+        Button buttonMyLocation = view.findViewById(R.id.buttonMyLocation);
+        buttonMyLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMapWithMyLocation();
+            }
+        });
+
+
         return view;
     }
 
@@ -73,5 +87,14 @@ public class FragmentInfo extends Fragment {
     private void navigateToArticle(String articleUrl) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(articleUrl));
         startActivity(intent);
+    }
+
+    private void openMapWithMyLocation() {
+        // Sử dụng Intent để mở ứng dụng Bản đồ
+        Uri geoLocation = Uri.parse("geo:0,0?q=your+location");
+        Intent intent = new Intent(Intent.ACTION_VIEW, geoLocation);
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
